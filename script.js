49c90edbb52608479fa604d56d0b35ad7b281cdf
WhiteHades <44260523+WhiteHades@users.noreply.github.com>
2024-01-19 21:58:32 +0100

    updated links, files.

--- script.js
+++ script.js
@@ -1,21 +1,25 @@
-let theme = localStorage.getItem('theme')
-if(theme == null){ setTheme('light') }
-else{ setTheme(theme) }
+let theme = localStorage.getItem('theme');
+if (theme == null) setTheme('light');
+else setTheme(theme);
 
-let themeDots = document.getElementsByClassName('theme-dot')
+let themeDots = document.getElementsByClassName('theme-dot');
 
-for (let i=0; themeDots.length > i; i++){
-	themeDots[i].addEventListener('click', function(){
-		let mode = this.dataset.mode
-		console.log('Option clicked:', mode)
-		setTheme(mode)
-	})
+for (let i = 0; themeDots.length > i; i++) {
+	themeDots[i].addEventListener('click', function() {
+		let mode = this.dataset.mode;
+		console.log('Option clicked:', mode);
+		setTheme(mode);
+	});
 }
 
-function setTheme(mode){
-	if(mode === 'light'){ document.getElementById('theme-style').href = 'default.css' }
-	if(mode === 'blue'){ document.getElementById('theme-style').href = 'blue.css' }
-	if(mode === 'green'){ document.getElementById('theme-style').href = 'green.css'}
-	if(mode === 'purple'){ document.getElementById('theme-style').href = 'purple.css'}
-	localStorage.setItem('theme', mode)
+function setTheme(mode) {
+	let stylesheetPath = '';
+	if (window.location.pathname.includes('projects')) stylesheetPath = '../';
+	if (mode === 'light') {
+		document.getElementById('theme-style').href = stylesheetPath + 'default.css';
+	}
+	if (mode === 'blue') {
+		document.getElementById('theme-style').href = stylesheetPath + 'blue.css';
+	}
+	localStorage.setItem('theme', mode);
 }
